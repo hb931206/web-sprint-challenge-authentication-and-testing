35013d905ea6135065639b4129c618ef27de60d4
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-07-17 19:37:04 -0400

    Created the register and login routes.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,59 @@
-const router = require('express').Router();
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Users = require("./auth-models");
 
-router.post('/register', (req, res) => {
-  // implement registration
+const router = express.Router();
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (user) {
+      return res.status(409).json({ message: "Username already taken" });
+    }
+    const newUser = await Users.add({
+      username,
+      password: await bcrypt.hash(password, 14),
+    });
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await Users.findBy({ username }).first();
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid Credentials",
+      });
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordValid) {
+      return res.status(401).json({
+        message: "Invalid Credentials",
+      });
+    }
+
+    const payload = {
+      userId: user.id,
+      username: user.username,
+    };
+
+    res.json({
+      message: `Welcome ${user.username}!`,
+      token: jwt.sign(payload, process.env.JWT_SECRET),
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
